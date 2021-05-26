c3c81ba6d40a883e8266cf8d94242c8a9880261c
Arnav Singh <arsing@microsoft.com>
2021-05-26 12:07:06 -0700

    Add ping example.
    This example subscribes to the same topic that it publishes,
    and prints how long it took to receive what it sent. This should be equal to
    common::BUFFER_TIME

--- examples/ping.rs
+++ examples/ping.rs
@@ -1,6 +1,6 @@
 // Example:
 //
-//     cargo run --release --example fast_publisher -- --payload-len 32
+//     cargo run --release --example ping -- --payload-len 32
 
 mod common;
 
@@ -22,7 +22,7 @@ fn main() {
     let Options {
         server,
         payload_len,
-    } = common::init("fast_publisher");
+    } = common::init("ping");
 
     let payload: bytes::Bytes = std::iter::repeat("1234567890").flat_map(str::as_bytes).take(payload_len).copied().collect();
     let payload = std::convert::TryInto::try_into(payload).unwrap();
@@ -44,32 +44,41 @@ fn main() {
     let () = stream.read_exact(&mut conn_ack).unwrap();
     assert_eq!(conn_ack, *b"\x20\x02\x00\x00");
 
+    let subscribe = encode(mqtt3::proto::Packet::Subscribe(mqtt3::proto::Subscribe {
+        packet_identifier: mqtt3::proto::PacketIdentifier::new(1).unwrap(),
+        subscribe_to: vec![mqtt3::proto::SubscribeTo {
+            topic_filter: "foo".to_owned().try_into().unwrap(),
+            qos: mqtt3::proto::QoS::AtMostOnce,
+        }],
+    }));
+    let () = stream.write_all(&*subscribe).unwrap();
+    let mut sub_ack = [0_u8; 5];
+    let () = stream.read_exact(&mut sub_ack).unwrap();
+    assert_eq!(sub_ack, *b"\x90\x03\x00\x01\x00");
+
     let publish = encode(mqtt3::proto::Packet::Publish(mqtt3::proto::Publish {
         packet_identifier_dup_qos: mqtt3::proto::PacketIdentifierDupQoS::AtMostOnce,
         retain: false,
         topic_name: "foo".to_owned().try_into().unwrap(),
         payload,
     }));
-    let mut publish_buf = Vec::with_capacity(8192);
+    let publish = &*publish;
 
-    let mut num_publish_packets_per_write = 0;
+    let mut recv = vec![0_u8; publish.len()];
     loop {
-        publish_buf.extend_from_slice(&publish);
-        num_publish_packets_per_write += 1;
-        if publish_buf.len() + publish.len() > 8192 {
-            break;
-        }
-    }
-
-    let publish = &*publish_buf;
-    log::info!("Writing {} packets ({} bytes) per write", num_publish_packets_per_write, publish.len());
-
-    let mut packet_stats: common::PacketStats = Default::default();
-
-    loop {
+        let start_send_time = std::time::Instant::now();
         let () = stream.write_all(publish).unwrap();
+        let finish_send_time = std::time::Instant::now();
 
-        packet_stats.count(num_publish_packets_per_write);
+        let () = stream.read_exact(&mut recv).unwrap();
+        let finish_recv_time = std::time::Instant::now();
+        assert_eq!(recv, *publish);
+        log::info!(
+            "start_send = {:?}, finish_send = {:?} (+{}ms), finish_recv = {:?} (+{}ms)",
+            start_send_time,
+            finish_send_time, (finish_send_time - start_send_time).as_millis(),
+            finish_recv_time, (finish_recv_time - finish_send_time).as_millis(),
+        );
     }
 }
 
