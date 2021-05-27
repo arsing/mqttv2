arsing/mqttv2

commit 5022bb182c6e78b58fd9941cac38551a437c3dfd

Break single event stream into separate streams for receiving, sending and accepting.
This allows prioritizing one action over the other. Specifically,
writes are now prioritized above everything else to get packets out of
the server, then accepts of new clients, and finally receives.

Sends are now buffered so that more IoSlices accumulate before being given to
poll_write_vectored. This increases smol's throughput
from ~830k packets/s to ~1300k packets/s. Unfortunately it appears to break
tokio - its write-flush futures seemingly never get scheduled.

Also spruce up the examples to use more common code for logging, packet stats
and the transport.

Lastly, add server address and payload length parameters to fast_publisher.

examples/common/transport/tokio.rs
@@ -1,4 +1,4 @@
-use bytes::{Buf, BufMut};
+use bytes::BufMut;
 
 #[cfg(feature = "client")]
 pub(crate) async fn connect(addr: impl tokio::net::ToSocketAddrs) -> std::io::Result<(impl mqtt3::io::PacketStream, impl mqtt3::io::PacketSink)> {
@@ -10,16 +10,13 @@ pub(crate) async fn connect(addr: impl tokio::net::ToSocketAddrs) -> std::io::Re
     let stream = IoStream {
         io: tokio::net::TcpStream::from_std(stream)?,
         decoder: Default::default(),
-        read_state: ReadState::WaitingForMore(bytes::BytesMut::with_capacity(1024)),
+        read_state: super::ReadState::WaitingForMore(bytes::BytesMut::with_capacity(1024)),
     };
 
     let sink = IoSink {
         io: tokio::net::TcpStream::from_std(sink)?,
-        write_state: WriteState {
-            in_progress: Default::default(),
-            prev: Default::default(),
-            curr: bytes::BytesMut::with_capacity(1024),
-        },
+        write_state: Default::default(),
+        buffer_timeout: Box::pin(tokio::time::sleep(super::BUFFER_TIME)),
     };
 
     Ok(mqtt3::io::logging(stream, sink))
@@ -55,16 +52,13 @@ impl mqtt3::io::Listener for Listener {
         let stream = IoStream {
             io: tokio::net::TcpStream::from_std(stream)?,
             decoder: Default::default(),
-            read_state: ReadState::WaitingForMore(bytes::BytesMut::with_capacity(1024)),
+            read_state: super::ReadState::WaitingForMore(bytes::BytesMut::with_capacity(1024)),
         };
 
         let sink = IoSink {
             io: tokio::net::TcpStream::from_std(sink)?,
-            write_state: WriteState {
-                in_progress: Default::default(),
-                prev: Default::default(),
-                curr: bytes::BytesMut::with_capacity(1024),
-            },
+            write_state: Default::default(),
+            buffer_timeout: Box::pin(tokio::time::sleep(super::BUFFER_TIME)),
         };
 
         std::task::Poll::Ready(Ok(mqtt3::io::logging(stream, sink)))
@@ -75,7 +69,7 @@ impl mqtt3::io::Listener for Listener {
 pub(crate) struct IoStream<Io> {
     #[pin] io: Io,
     decoder: mqtt3::proto::PacketDecoder,
-    read_state: ReadState,
+    read_state: super::ReadState,
 }
 
 impl<Io> futures_core::Stream for IoStream<Io> where Io: tokio::io::AsyncRead {
@@ -86,7 +80,7 @@ impl<Io> futures_core::Stream for IoStream<Io> where Io: tokio::io::AsyncRead {
 
         loop {
             match this.read_state {
-                ReadState::WaitingForMore(buf) => {
+                super::ReadState::WaitingForMore(buf) => {
                     let mut read_buf = as_read_buf(buf);
 
                     match this.io.as_mut().poll_read(cx, &mut read_buf)? {
@@ -100,24 +94,19 @@ impl<Io> futures_core::Stream for IoStream<Io> where Io: tokio::io::AsyncRead {
                     }
 
                     unsafe { buf.advance_mut(read); }
-                    *this.read_state = ReadState::MightBeEnough(std::mem::take(buf));
+                    *this.read_state = super::ReadState::MightBeEnough(std::mem::take(buf));
                 },
 
-                ReadState::MightBeEnough(buf) => {
+                super::ReadState::MightBeEnough(buf) => {
                     if let Some(packet) = mqtt3::proto::decode(this.decoder, buf)? {
                         return std::task::Poll::Ready(Some(Ok(packet)));
                     }
 
-                    *this.read_state = ReadState::WaitingForMore(std::mem::take(buf));
+                    *this.read_state = super::ReadState::WaitingForMore(std::mem::take(buf));
                 },
             }
         }
     }
-}
-
-enum ReadState {
-    WaitingForMore(bytes::BytesMut),
-    MightBeEnough(bytes::BytesMut),
 }
 
 fn as_read_buf(buf: &'_ mut bytes::BytesMut) -> tokio::io::ReadBuf<'_> {
@@ -142,14 +131,20 @@ fn as_read_buf(buf: &'_ mut bytes::BytesMut) -> tokio::io::ReadBuf<'_> {
 #[pin_project::pin_project]
 pub(crate) struct IoSink<Io> {
     #[pin] io: Io,
-    write_state: WriteState,
+    write_state: super::WriteState,
+    buffer_timeout: std::pin::Pin<Box<tokio::time::Sleep>>,
 }
 
 impl<Io> futures_sink::Sink<mqtt3::proto::Packet> for IoSink<Io> where Io: tokio::io::AsyncWrite {
     type Error = mqtt3::proto::EncodeError;
 
-    fn poll_ready(self: std::pin::Pin<&mut Self>, _cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
-        std::task::Poll::Ready(Ok(()))
+    fn poll_ready(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        if self.as_mut().project().write_state.prev.len() < super::NUM_IO_SLICES {
+            std::task::Poll::Ready(Ok(()))
+        }
+        else {
+            self.poll_flush(cx)
+        }
     }
 
     fn start_send(self: std::pin::Pin<&mut Self>, item: mqtt3::proto::Packet) -> Result<(), Self::Error> {
@@ -161,20 +156,23 @@ impl<Io> futures_sink::Sink<mqtt3::proto::Packet> for IoSink<Io> where Io: tokio
     fn poll_flush(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
         let mut this = self.project();
 
-        loop {
-            {
-                let mut dst = [std::io::IoSlice::new(b""); 64];
+        if this.write_state.prepare_for_write() {
+            if this.write_state.prev.len() < super::NUM_IO_SLICES {
+                use std::future::Future;
+
+                match this.buffer_timeout.as_mut().poll(cx) {
+                    std::task::Poll::Ready(()) => (),
+                    std::task::Poll::Pending => return std::task::Poll::Pending,
+                }
+            }
+
+            while this.write_state.prepare_for_write() {
+                let mut dst = [std::io::IoSlice::new(b""); super::NUM_IO_SLICES];
                 let num_chunks = this.write_state.chunks_vectored(&mut dst);
-                if num_chunks > 0 {
-                    match this.io.as_mut().poll_write_vectored(cx, &dst[..num_chunks])? {
-                        std::task::Poll::Ready(0) => {
-                            return std::task::Poll::Ready(Err(std::io::Error::from(std::io::ErrorKind::UnexpectedEof).into()));
-                        },
-                        std::task::Poll::Ready(written) => {
-                            this.write_state.advance(written);
-                        },
-                        std::task::Poll::Pending => return std::task::Poll::Pending,
-                    }
+                match this.io.as_mut().poll_write_vectored(cx, &dst[..num_chunks])? {
+                    std::task::Poll::Ready(0) => return std::task::Poll::Ready(Err(std::io::Error::from(std::io::ErrorKind::UnexpectedEof).into())),
+                    std::task::Poll::Ready(written) => this.write_state.advance(written),
+                    std::task::Poll::Pending => return std::task::Poll::Pending,
                 }
             }
 
@@ -182,12 +180,9 @@ impl<Io> futures_sink::Sink<mqtt3::proto::Packet> for IoSink<Io> where Io: tokio
                 std::task::Poll::Ready(()) => (),
                 std::task::Poll::Pending => return std::task::Poll::Pending,
             }
-
-            if this.write_state.in_progress.is_empty() {
-                break;
-            }
         }
 
+        this.buffer_timeout.as_mut().reset(tokio::time::Instant::now() + super::BUFFER_TIME);
         std::task::Poll::Ready(Ok(()))
     }
 
@@ -203,58 +198,5 @@ impl<Io> futures_sink::Sink<mqtt3::proto::Packet> for IoSink<Io> where Io: tokio
         }
 
         std::task::Poll::Ready(Ok(()))
-    }
-}
-
-struct WriteState {
-    in_progress: std::collections::VecDeque<bytes::Bytes>,
-    prev: std::collections::VecDeque<bytes::Bytes>,
-    curr: bytes::BytesMut,
-}
-
-impl WriteState {
-    fn chunks_vectored<'a>(&'a mut self, dst: &mut [std::io::IoSlice<'a>]) -> usize {
-        if self.in_progress.is_empty() {
-            std::mem::swap(&mut self.in_progress, &mut self.prev);
-            if !self.curr.is_empty() {
-                self.in_progress.push_back(self.curr.split().freeze());
-            }
-        }
-
-        for (dst, src) in dst.iter_mut().zip(&self.in_progress) {
-            *dst = std::io::IoSlice::new(&**src);
-        }
-        std::cmp::min(self.in_progress.len(), dst.len())
-    }
-
-    fn advance(&mut self, mut cnt: usize) {
-        while let Some(mut buf) = self.in_progress.pop_front() {
-            if cnt < buf.len() {
-                buf.advance(cnt);
-                self.in_progress.push_front(buf);
-                cnt = 0;
-                break;
-            }
-
-            cnt -= buf.len();
-        }
-        assert_eq!(cnt, 0);
-    }
-}
-
-impl mqtt3::proto::ByteBuf for WriteState {
-    fn put_u8_bytes(&mut self, n: u8) {
-        self.curr.put_u8(n);
-    }
-
-    fn put_u16_bytes(&mut self, n: u16) {
-        self.curr.put_u16(n);
-    }
-
-    fn put_bytes(&mut self, src: bytes::Bytes) {
-        if !self.curr.is_empty() {
-            self.prev.push_back(std::mem::take(&mut self.curr).freeze());
-        }
-        self.prev.push_back(src);
     }
 }

examples/server.rs
@@ -16,7 +16,10 @@ struct Options {
 
 #[cfg(feature = "transport-smol")]
 fn main() {
-    let () = smol::block_on(main_inner());
+    let bind = init();
+    let listener = smol::block_on(common::transport::smol::Listener::bind(bind)).expect("bind failed");
+
+    let () = smol::block_on(mqtt3::server::run(listener)).expect("server failed");
 }
 
 #[cfg(feature = "transport-tokio")]
@@ -24,29 +27,19 @@ fn main() {
     let runtime =
         tokio::runtime::Builder::new_current_thread()
         .enable_io()
+        .enable_time()
         .build().expect("could not create runtime");
-
     let local_set = tokio::task::LocalSet::new();
 
-    let () = local_set.block_on(&runtime, tokio::task::unconstrained(main_inner()));
+    let bind = init();
+    let listener = local_set.block_on(&runtime, common::transport::tokio::Listener::bind(bind)).expect("bind failed");
+
+    let () = local_set.block_on(&runtime, tokio::task::unconstrained(mqtt3::server::run(listener))).expect("server failed");
 }
 
-async fn main_inner() {
-    env_logger::Builder::from_env(env_logger::Env::new().filter_or(
-        "MQTT3_LOG",
-        "mqtt3=debug,mqtt3::io=trace,server=info",
-    ))
-    .init();
-
+fn init() -> std::net::SocketAddr {
     let Options {
         bind,
-    } = structopt::StructOpt::from_args();
-
-    #[cfg(feature = "transport-smol")]
-    let listener = common::smol::Listener::bind(bind).await.expect("bind failed");
-
-    #[cfg(feature = "transport-tokio")]
-    let listener = common::tokio::Listener::bind(bind).await.expect("bind failed");
-
-    let () = mqtt3::server::run(listener).await.expect("server failed");
+    } = common::init("server");
+    bind
 }

Cargo.toml
@@ -17,13 +17,14 @@ futures-sink = { version = "0.3", optional = true, default-features = false }
 futures-util = { version = "0.3", optional = true, default-features = false, features = [
 	"sink",
 ] }
-log = { version = "0.4", optional = true, default-features = false }
+log = { version = "0.4", default-features = false }
 pin-project = { version = "1", optional = true, default-features = false }
 smol = { version = "1", optional = true, default-features = false }
 tokio = { version = "1", optional = true, default-features = false }
 
 [dev-dependencies]
 env_logger = { version = "0.8", default-features = false, features = ["atty", "humantime", "termcolor"] }
+heapless = { version = "0.7", default-features = false }
 structopt = { version = "0.3", default-features = false }
 tokio = { version = "1", default-features = false, features = ["macros", "net", "rt", "rt-multi-thread", "signal"] }
 
@@ -46,12 +47,12 @@ transport-smol = [
 transport-tokio = [
 	"tokio",
 	"tokio/net",
+	"tokio/time",
 ]
 _common = [
 	"futures-core",
 	"futures-sink",
 	"futures-util",
-	"log",
 	"pin-project",
 ]
 

examples/common/mod.rs
@@ -1,8 +1,18 @@
-#[cfg(feature = "transport-smol")]
-pub(crate) mod smol;
+#[cfg(any(
+    feature = "transport-smol",
+    feature = "transport-tokio",
+))]
+pub(crate) mod transport;
 
-#[cfg(feature = "transport-tokio")]
-pub(crate) mod tokio;
+pub(crate) fn init<Options>(example_name: &str) -> Options where Options: structopt::StructOpt {
+    env_logger::Builder::from_env(env_logger::Env::new().filter_or(
+        "MQTT3_LOG",
+        &format!("mqtt3=info,{example_name}=info", example_name = example_name),
+    ))
+    .init();
+
+    structopt::StructOpt::from_args()
+}
 
 #[allow(dead_code)]
 pub(crate) fn duration_from_secs_str(
@@ -21,5 +31,49 @@ pub(crate) fn qos_from_str(s: &str) -> Result<mqtt3::proto::QoS, String> {
             "unrecognized QoS {:?}: must be one of 0, 1, 2, AtMostOnce, AtLeastOnce, ExactlyOnce",
             s
         )),
+    }
+}
+
+pub(crate) const PACKET_STATS_WINDOW_SIZE: usize = 60;
+
+#[allow(dead_code)]
+pub(crate) struct PacketStats {
+    start_time: std::time::Instant,
+    current: u128,
+    history: heapless::HistoryBuffer<u128, PACKET_STATS_WINDOW_SIZE>,
+}
+
+impl PacketStats {
+    #[allow(dead_code)]
+    pub(crate) fn count(&mut self, num: usize) {
+        self.current += num as u128;
+
+        let now = std::time::Instant::now();
+        let elapsed = now.duration_since(self.start_time);
+        if elapsed > std::time::Duration::from_secs(1) {
+            self.history.write(self.current);
+
+            log::info!(
+                "{:>8} | 1s average: {:>8}/s) | {}s average: {:>8}/s{}",
+                self.current,
+                self.current * 1_000_000 / elapsed.as_micros(),
+                PACKET_STATS_WINDOW_SIZE,
+                self.history.as_slice().iter().sum::<u128>() * 1_000_000 / (self.history.len() as u128) / elapsed.as_micros(),
+                if self.history.len() < PACKET_STATS_WINDOW_SIZE { " (?)" } else { "" },
+            );
+
+            self.current = 0;
+            self.start_time = now;
+        }
+    }
+}
+
+impl Default for PacketStats {
+    fn default() -> Self {
+        PacketStats {
+            start_time: std::time::Instant::now(),
+            current: 0,
+            history: heapless::HistoryBuffer::new(),
+        }
     }
 }

examples/common/transport/mod.rs
@@ -1,0 +1,79 @@
+use bytes::{Buf, BufMut};
+
+#[cfg(feature = "transport-smol")]
+pub(crate) mod smol;
+
+#[cfg(feature = "transport-tokio")]
+pub(crate) mod tokio;
+
+enum ReadState {
+    WaitingForMore(bytes::BytesMut),
+    MightBeEnough(bytes::BytesMut),
+}
+
+struct WriteState {
+    prev: std::collections::VecDeque<bytes::Bytes>,
+    curr: bytes::BytesMut,
+}
+
+impl WriteState {
+    /// Returns true if there is something to write.
+    fn prepare_for_write(&mut self) -> bool {
+        if !self.curr.is_empty() && self.prev.len() < NUM_IO_SLICES {
+            self.prev.push_back(self.curr.split().freeze());
+        }
+
+        !self.prev.is_empty()
+    }
+
+    fn chunks_vectored<'a>(&'a self, dst: &mut [std::io::IoSlice<'a>]) -> usize {
+        for (dst, src) in dst.iter_mut().zip(&self.prev) {
+            *dst = std::io::IoSlice::new(&**src);
+        }
+        std::cmp::min(self.prev.len(), dst.len())
+    }
+
+    fn advance(&mut self, mut cnt: usize) {
+        while let Some(mut buf) = self.prev.pop_front() {
+            if cnt < buf.len() {
+                buf.advance(cnt);
+                self.prev.push_front(buf);
+                cnt = 0;
+                break;
+            }
+
+            cnt -= buf.len();
+        }
+        assert_eq!(cnt, 0);
+    }
+}
+
+impl Default for WriteState {
+    fn default() -> Self {
+        WriteState {
+            // prev will receive at least one more packet even when it has NUM_IO_SLICES buffers, so space for more buffers
+            prev: std::collections::VecDeque::with_capacity(NUM_IO_SLICES * 2),
+            curr: bytes::BytesMut::with_capacity(1024),
+        }
+    }
+}
+
+impl mqtt3::proto::ByteBuf for WriteState {
+    fn put_u8_bytes(&mut self, n: u8) {
+        self.curr.put_u8(n);
+    }
+
+    fn put_u16_bytes(&mut self, n: u16) {
+        self.curr.put_u16(n);
+    }
+
+    fn put_bytes(&mut self, src: bytes::Bytes) {
+        if !self.curr.is_empty() {
+            self.prev.push_back(std::mem::take(&mut self.curr).freeze());
+        }
+        self.prev.push_back(src);
+    }
+}
+
+const NUM_IO_SLICES: usize = 128;
+const BUFFER_TIME: std::time::Duration = std::time::Duration::from_millis(500);

commit 74eaaf355a483c6cf9fa75531e5161261d2e54bc

Fix transport-tokio server and bring the code in line with current transport-smol.
5022bb182c6e78b58fd9941cac38551a437c3dfd broke tokio when it introduced
send-buffering using a timer. This commit fixes the breakage.

The earlier optimization of disabling coop budget on the server future also
made it so that tokio's timer wheel never got scheduled as long as IO was
keeping the executor busy, and thus the wheel was never able to mark
elapsed timers as elapsed. Thus even if the timers were polled explicitly,
they would believe they weren't elapsed yet, so the sinks would
never get flushed.

examples/common/transport/tokio.rs
@@ -16,7 +16,7 @@ pub(crate) async fn connect(addr: impl tokio::net::ToSocketAddrs) -> std::io::Re
     let sink = IoSink {
         io: tokio::net::TcpStream::from_std(sink)?,
         write_state: Default::default(),
-        buffer_timeout: Box::pin(tokio::time::sleep(super::BUFFER_TIME)),
+        buffer_timeout: None,
     };
 
     Ok(mqtt3::io::logging(stream, sink))
@@ -58,7 +58,7 @@ impl mqtt3::io::Listener for Listener {
         let sink = IoSink {
             io: tokio::net::TcpStream::from_std(sink)?,
             write_state: Default::default(),
-            buffer_timeout: Box::pin(tokio::time::sleep(super::BUFFER_TIME)),
+            buffer_timeout: None,
         };
 
         std::task::Poll::Ready(Ok(mqtt3::io::logging(stream, sink)))
@@ -132,7 +132,7 @@ fn as_read_buf(buf: &'_ mut bytes::BytesMut) -> tokio::io::ReadBuf<'_> {
 pub(crate) struct IoSink<Io> {
     #[pin] io: Io,
     write_state: super::WriteState,
-    buffer_timeout: std::pin::Pin<Box<tokio::time::Sleep>>,
+    buffer_timeout: Option<std::pin::Pin<Box<tokio::time::Sleep>>>,
 }
 
 impl<Io> futures_sink::Sink<mqtt3::proto::Packet> for IoSink<Io> where Io: tokio::io::AsyncWrite {
@@ -160,19 +160,23 @@ impl<Io> futures_sink::Sink<mqtt3::proto::Packet> for IoSink<Io> where Io: tokio
             if this.write_state.prev.len() < super::NUM_IO_SLICES {
                 use std::future::Future;
 
-                match this.buffer_timeout.as_mut().poll(cx) {
+                let buffer_timeout = this.buffer_timeout.get_or_insert_with(|| Box::pin(tokio::time::sleep(super::BUFFER_TIME)));
+                match buffer_timeout.as_mut().poll(cx) {
                     std::task::Poll::Ready(()) => (),
                     std::task::Poll::Pending => return std::task::Poll::Pending,
                 }
             }
 
-            while this.write_state.prepare_for_write() {
+            loop {
                 let mut dst = [std::io::IoSlice::new(b""); super::NUM_IO_SLICES];
                 let num_chunks = this.write_state.chunks_vectored(&mut dst);
                 match this.io.as_mut().poll_write_vectored(cx, &dst[..num_chunks])? {
                     std::task::Poll::Ready(0) => return std::task::Poll::Ready(Err(std::io::Error::from(std::io::ErrorKind::UnexpectedEof).into())),
                     std::task::Poll::Ready(written) => this.write_state.advance(written),
                     std::task::Poll::Pending => return std::task::Poll::Pending,
+                }
+                if !this.write_state.prepare_for_write() {
+                    break;
                 }
             }
 
@@ -182,7 +186,7 @@ impl<Io> futures_sink::Sink<mqtt3::proto::Packet> for IoSink<Io> where Io: tokio
             }
         }
 
-        this.buffer_timeout.as_mut().reset(tokio::time::Instant::now() + super::BUFFER_TIME);
+        *this.buffer_timeout = None;
         std::task::Poll::Ready(Ok(()))
     }
 

examples/server.rs
@@ -35,7 +35,7 @@ fn main() {
     let bind = init();
     let listener = local_set.block_on(&runtime, common::transport::tokio::Listener::bind(bind)).expect("bind failed");
 
-    let () = local_set.block_on(&runtime, tokio::task::unconstrained(mqtt3::server::run(listener))).expect("server failed");
+    let () = local_set.block_on(&runtime, mqtt3::server::run(listener)).expect("server failed");
 }
 
 fn init() -> std::net::SocketAddr {